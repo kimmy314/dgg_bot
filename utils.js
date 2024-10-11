226cbeaa9304396c99f8c38daa7dbd1318c6844c
Unremarkable <andrew.hausch@gmail.com>
2024-10-11 13:33:35 -0700

    Fix a small issue with the `formatAsTable`

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ function formatAsTable(headers, rows) {
     const widths = headers.map((header, index) => Math.max(...[header, ...rows.map(row => row[index])].map(s => s.length)));
 
     const header = headers.map((header, index) => header.padStart(widths[index])).join(' | ')
-    const divider = new Array(widths.reduce((acc, a) => acc + a, 0) + (widths.length * 3)).fill('-').join('');
+    const divider = new Array(widths.reduce((acc, a) => acc + a, 0) + ((widths.length - 1) * 3)).fill('-').join('');
     const body = rows.map(row => row.map((string, index) => string.padStart(widths[index])).join(' | ')).join('\n');
 
     return '```' + `${header}\n${divider}\n${body}` + '```';
